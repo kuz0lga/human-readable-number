8f4139f40ac96730676b3c107472e9354f8ed9fc
kuz0lga <kuz0lga@yandex.ru>
2022-06-28 14:42:47 +0300

    add: added task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
 module.exports = function toReadable (number) {
+ 
+    const num =  [
+        'zero',
+        'one',
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",];
+
+    const dozens = [
+        '',
+        'ten',
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",];
+    
   
+if (number < 100) {
+  if (number < 20) {
+      return num[number];
+  }
+  
+  else if (number % 10 === 0) {
+    return dozens[Math.floor(number / 10)];
+  }
+
+  else  {
+    return (dozens[Math.floor(number / 10)] + ' ' + num[number % 10]);
+  }
 }
+
+  else  {
+      if (number % 10 !== 0 && number % 100 !== 0) {
+      return (num[Math.floor(number / 100)] + ' hundred ' + dozens[(Math.floor(number / 10)) % 10] + ' ' +num[number % 10]);
+      
+  }
+      else if (number % 10 === 0) {
+        return (num[Math.floor(number / 100)] + ' hundred ' + dozens[(Math.floor(number / 10)) % 10]);
+      } 
+      else if (number % 100 < 20) {
+        return (num[Math.floor(number / 100)] + ' hundred ' +num[number % 100]);
+      }
+      else {
+        return (num[Math.floor(number / 100)] + ' hundred');
+      }
+  }
+}
+
+
